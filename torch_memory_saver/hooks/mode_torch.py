410cfb0bdbb3d74ed5190681e82d51fa62fede08
zhaoping <zpinghu@gmail.com>
2026-01-22 17:53:28 +0800

    add musa patch
    patch CUDAPluggableAllocator with MUSAPluggableAllocator.

--- torch_memory_saver/hooks/mode_torch.py
+++ torch_memory_saver/hooks/mode_torch.py
@@ -2,14 +2,28 @@ import logging
 
 from torch_memory_saver.hooks.base import HookUtilBase
 from torch_memory_saver.utils import get_binary_path_from_package
-from torch.cuda.memory import CUDAPluggableAllocator
+import torch
+
+def get_pluggable_allocator():
+    # CUDA
+    if hasattr(torch, "cuda") and torch.cuda.is_available():
+        from torch.cuda.memory import CUDAPluggableAllocator
+        return CUDAPluggableAllocator
+
+    # MUSA
+    if hasattr(torch, "musa") and torch.musa.is_available():
+        return torch.musa.memory.MUSAPluggableAllocator
+
+    raise RuntimeError("No supported backend (CUDA/MUSA) available for pluggable allocator.")
+
+PluggableAllocator = get_pluggable_allocator()
 
 logger = logging.getLogger(__name__)
 
 
 class HookUtilModeTorch(HookUtilBase):
     def __init__(self):
-        self.allocator = CUDAPluggableAllocator(self.get_path_binary(), "tms_torch_malloc", "tms_torch_free")
+        self.allocator = PluggableAllocator(self.get_path_binary(), "tms_torch_malloc", "tms_torch_free")
         logger.debug(f"HookUtilModeTorch {self.allocator=} {self.get_path_binary()=}")
 
     def get_path_binary(self):
